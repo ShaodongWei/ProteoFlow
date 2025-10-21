1524515a4d0ae2b3841ba579f2883c4d3f6f561c
ShaodongWei <43374853+ShaodongWei@users.noreply.github.com>
2025-10-21 11:49:22 +0200

    Update abundances_counts.py
    Refactor script to load data and perform statistical analysis. Added functions for testing features and plotting results.

--- scripts/abundances_counts.py
+++ scripts/abundances_counts.py
@@ -1,55 +1,80 @@
 import pandas as pd 
 import os
+import numpy as np 
+import statsmodels.api as sm
+from statsmodels.formula.api import ols
+from joblib import Parallel, delayed
+import statsmodels.api as sm
+from tqdm import tqdm
+import re
+import plotly.express as px
+
 
 ## make directory 
 os.makedirs(snakemake.params.output_dir, exist_ok=True)
 
-## load cleaned data
-data = pd.read_table(snakemake.input.data, sep='\t', index_col=0)
-# data = pd.read_csv('./data_input/cleaned_data.tsv', sep='\t', index_col=0)
-data_long = pd.melt(data.reset_index(names='sample'), id_vars='sample', var_name='feature', value_name='abundance')
+## load cleaned pg table 
+data = pd.read_table(snakemake.params.data, sep='\t', index_col=0).reset_index(names='sample')
+meta = pd.read_table(snakemake.params.meta, sep='\t', index_col=0).reset_index(names='sample')
 
-## calculate median abundance for each feature
-df_abundance = data.median().reset_index()
-df_abundance.columns = ['feature','abundance']
-df_abundance = df_abundance.sort_values('abundance', ascending=False).reset_index(drop=True)
-df_abundance['ranks'] = df_abundance.index + 1
+## reshape and merge
+data_long = data.melt(id_vars='sample', value_name='abundance', var_name='feature')
+df = pd.merge(data_long, meta, on='sample', how='inner')
 
-## calculate sample feature counts 
-df_raw = pd.read_csv('./data_input/raw_data.tsv', sep='\t', index_col=0)
-df_raw = df_raw.count(axis=1).reset_index(name='count').sort_values('count', ascending=False).reset_index(drop=True)
-df_raw['ranks'] = df_raw.index + 1
-df_raw.head()
+def test_feature_series(value_series, group_series):
+    if len(group_series.unique()) != 2:
+        raise ValueError('only two groups are supported')    
+    model = ols(f'value ~ C(type)', data=pd.DataFrame({'value':value_series, 'type':group_series})).fit()
+    anova_table = sm.stats.anova_lm(model)
+    pval = anova_table['PR(>F)'].iloc[0].round(4)
+    fold = np.log2(value_series[group_series == group_series.unique()[0]].mean()/value_series[group_series == group_series.unique()[1]].mean())
+    fold = np.round(fold,3)
+    level = group_series.unique()[0] + '/' + group_series.unique()[1]
+    return {'log2foldratio': fold, 'pval': pval, 'level': level}
 
-## scatter plot
-from plotly.subplots import make_subplots
-import plotly.graph_objects as go 
+def test_feature_dataframe(dataframe, feature_col, value_col, group_col, n_jobs=1):
+    if dataframe[group_col].nunique() != 2:
+        raise ValueError('only two groups are supported')
+    features = dataframe[feature_col].unique()
+    ot =  Parallel(n_jobs) (
+        delayed(test_feature_series)(value, group) for value, group in 
+                [(dataframe[dataframe[feature_col]==feature][value_col], dataframe[dataframe[feature_col]==feature][group_col]) 
+                 for feature in tqdm(features)]
+    )
+    ot = pd.DataFrame(ot)
+    ot['pval_adj'] = sm.stats.multipletests(ot['pval'],method='fdr_bh')[1].round(4)
+    ot['feature'] = features
+    return ot
 
-fig = make_subplots(rows=2, cols=1, 
-                    subplot_titles=['Feature abundance', 'Number of detected features per sample'])
+df_sig = test_feature_dataframe(df, feature_col='feature', value_col='abundance', group_col=snakemake.params.group_column, n_jobs=snakemake.params.threads)
+df_sig.to_csv(f"{snakemake.params.output_dir}/df_differential_test.tsv", sep='\t', index=False)
 
-fig.add_trace(
-    go.Scatter(
-    x=df_abundance['ranks'],
-    y=df_abundance['abundance'],
-    ),
-    row=1, col=1
-)
-fig.add_trace(
-    go.Bar(
-    x=df_raw['ranks'],
-    y=df_raw['count'],
-    marker=dict(color='gray')
-    ),
-    row=2,col=1
+## plot
+df_sig['Significance'] = df_sig['pval_adj'].apply(lambda x: 'Significant' if x < 0.05 else 'Non-significant')
+
+raw_offset = df_sig.loc[df_sig['pval_adj'] > 0, 'pval_adj'].min() / 10
+offset = 10 ** np.floor(np.log10(raw_offset))
+print(offset)
+level = re.sub('>', '/', df_sig['level'].values[0])
+df_sig['y'] = df_sig['pval_adj'].apply(lambda x: -np.log10(offset) if x == 0 else -np.log10(x))
+fig = px.scatter(df_sig, x='log2foldratio', y='y', color='Significance')
+fig.add_vline(
+    x=0,
+    line_color='gray',
+    line_dash='dash'
 )
 fig.update_layout(
-    showlegend=False
+    yaxis_title="-log10(adjusted p-value)",
+    xaxis_title=f"log2(fold change {level})",
 )
-fig.update_xaxes(title_text='Sample ranks', row=2, col=1)
-fig.update_xaxes(title_text='Feature ranks', row=1, col=1)
-fig.write_html(f"{snakemake.params.output_dir}/abundances_counts.html")
+fig.update_yaxes(
+    range=[0,-np.log10(offset)+0.5], 
+    tickvals=[i for i in range(0,int(-np.log10(offset)+1))],
+    ticktext=[str(i) for i in range(0,int(-np.log10(offset)+1))],
+    )
 
-# rule finishes 
+fig.write_html(f"{snakemake.params.output_dir}/volcano.html")
+
+## rule finishes
 with open(snakemake.output[0],'w') as f:
     f.write('')
